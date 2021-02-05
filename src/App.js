8de98c5074f2cc691d6abc43bc099552d3f99234
Eddie Herbert <abeh78@gmail.com>
2021-02-05 14:20:18 -0500

    updated the words

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Quickly
+          This is my amazing website
         </a>
       </header>
     </div>
