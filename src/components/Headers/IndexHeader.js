c9fd5147ecf2c12beb0f158c4403385397ecb230
Eddie Herbert <abeh78@gmail.com>
2021-03-10 14:52:04 -0500

    use ui kit version var

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -40,7 +40,7 @@ function IndexHeader() {
               src={require("assets/img/now-logo.png")}
             ></img>
             <h1 className="h1-seo">Now UI Kit.</h1>
-            <h3>A beautiful Bootstrap {process.env.UI_KIT_VERSION || "SHIT"} UI kit. Yours free.</h3>
+            <h3>A beautiful Bootstrap {process.env.UI_KIT_VERSION} UI kit. Yours free.</h3>
           </div>
           <h6 className="category category-absolute">
             Designed by{" "}
